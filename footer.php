0cca1bd78f22c7c35417727db50c1967814a087e
Aaron Yarborough <personal.ajy@gmail.com>
2019-01-14 22:54:54 +0000

    REMOVE

--- footer.php
+++ footer.php
@@ -4,11 +4,6 @@
             <div class="col-sm-12">
                 <p><strong>Information</strong></p>
                 <p>If you wish to use any of the content on this site for your project, please consider crediting the original authors in some way.</p>
-                
-                <p style="font-size: 12px">
-                    Site by Aaron Yarborough <br/>
-                    <a href="mailto:graalarchive@aaronjy.me">Contact me</a>
-                </p>
             </div>
         </div>
     </div>
