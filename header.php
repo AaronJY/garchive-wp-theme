c29f5f4905d0fcf7c00e9d2d10e55f54228e2962
Aaron Yarborough <personal.ajy@gmail.com>
2019-01-15 19:28:27 +0000

    Comment out login

--- header.php
+++ header.php
@@ -6,13 +6,12 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75664743-2"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-75664743-2');
+        gtag('config', 'UA-75664743-2');
     </script>
-
 
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -24,18 +23,19 @@
 <body <?php body_class(); ?>>
 
 <?php
-$curr_user = wp_get_current_user();
+    $curr_user = wp_get_current_user();
 ?>
 
-
-    <div class="gar-userbox">
-        <?php if ($curr_user->ID !== 0) : ?>
-            <span class="gar-userbox-username"><?php echo $curr_user->user_login ?></span>
-            <a href="<?php echo wp_logout_url(esc_url(home_url('/'))) ?>">Logout</a>
-        <?php else : ?>
-            <a href="<?php echo wp_login_url(esc_url(home_url('/'))) ?>">Login</a>
-        <?php endif; ?>
-    </div>
+<?php /*
+<div class="gar-userbox">
+    <?php if ($curr_user->ID !== 0) : ?>
+        <span class="gar-userbox-username"><?php echo $curr_user->user_login ?></span>
+        <a href="<?php echo wp_logout_url(esc_url(home_url('/'))) ?>">Logout</a>
+    <?php else : ?>
+        <a href="<?php echo wp_login_url(esc_url(home_url('/'))) ?>">Login</a>
+    <?php endif; ?>
+</div>
+*/ ?>
 
 
 <div id="search">
